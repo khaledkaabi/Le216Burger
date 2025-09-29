064516393b5b8e64ae2272605d56b6f3d027e593
kkaabi-sairen <khaled.kaabi@sairen.io>
2025-09-29 22:14:48 +0100

    add sitemap & robot

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import { useSEO } from './hooks/useSeo';
 import Home from './page';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const AppRoutes = () => {
+
+    useSEO({
+    title: "Le 216 Burger",
+    description: "Bienvenue chez Le 216 Burger, plaisir et saveurs garantis.",
+    url: "https://le216-burger.vercel.app/",
+  });
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
